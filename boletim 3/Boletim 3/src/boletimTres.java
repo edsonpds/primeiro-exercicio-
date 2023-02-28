b69c0d44ea1e75f74e2af3e246b9b46d56b00a66
Edson Peres <123039196+edsonpds@users.noreply.github.com>
2023-02-27 23:04:33 -0300

    Add files via upload

---
+++ boletim 3/Boletim 3/src/boletimTres.java
@@ -0,0 +1,16 @@
+public class boletimTres {
+    public static void main(String[] args) throws Exception {
+       String nome = "Zézinho";
+       System.out.println(nome);
+       int n1 = 5;
+       int n2 = 6;
+       int mediaMínima  = 7;
+       int total = n1 + n2;
+
+       if (total > mediaMínima)
+       System.out.println("Aprovado"); 
+    
+
+
+}
+}
